fa52df123deffef2daf6821a905d92e6841b4dea
slz <42647409+capricorn2121@users.noreply.github.com>
2024-01-02 11:38:10 -0500

    Update TomoFrame.java
    added the other panel stuff

--- TomoFrame.java
+++ TomoFrame.java
@@ -11,7 +11,15 @@ public class TomoFrame extends JFrame{
 	TomoPanel panel;
 	
 	public TomoFrame() {
-		
+		panel = new TomoPanel(); // run TomoPanel constructor
+		this.add(panel);
+		this.setTitle("TOMOTASKER"); // set title for frame
+		this.setResizable(false); // frame can't change size
+		this.setBackground(Color.white); // sets background color for the frame
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button will stop program execution
+		this.pack();// makes components fit in window
+		this.setVisible(true); // makes window visible to user
+		this.setLocationRelativeTo(null);// set window in middle of screen
 	}
 	
 }
