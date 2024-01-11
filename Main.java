e5b606b188059e5dc05189734ce5c92422d0f278
slz <42647409+capricorn2121@users.noreply.github.com>
2024-01-11 17:50:42 -0500

    Update Main.java with comments

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
-/* Elaine Qian and Shiloh Zheng
- * TomoTasker
- * write more stuff later lol
- */
+/* Shiloh Zheng and Elaine Qian
+ * January 11th, 2024
+ * Main
+ * This class runs the constructor of the TomoFrame class
+*/
 
 public class Main {
 
 	public static void main(String[] args) {
+		// runs the constructor of the TomoFrame class
 		new TomoFrame();
-		// hi elaine :D
 	}
 
 }
