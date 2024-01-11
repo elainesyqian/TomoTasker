11f7a1f1387724890b5d93cc2ce7e794e0c262ca
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-11 18:34:11 -0500

    added comments, still doesnt work :(

--- StreakCounter.java
+++ StreakCounter.java
@@ -7,38 +7,36 @@ import java.text.SimpleDateFormat;
 
 public class StreakCounter {
 
-	public Scanner fileScan;
-	public FileWriter fileSave;
-	
-	public static File file = new File("loginStreak.txt").getAbsoluteFile();
+	public static File file;
 	public static BufferedWriter bw;
-	public BufferedReader br;
+	public static BufferedReader br;
 
 	public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	
+
 	public static Date todaysDate;
 	public static String today;
 	public String str;
-	
+
 	public Date dayBefore;
 	public Calendar calendar;
-	
+
 	public int curStreak;
+	public StreakCounter streak;
 
 	public StreakCounter() throws ParseException, IOException {
-
-		getToday();
-		
-		curStreak = calculateStreakLength();
-		
-		if (!checkToday()) {
-			bw.write(today);
-		}
 
 		file = new File("loginStreak.txt").getAbsoluteFile();
 		bw = new BufferedWriter(new FileWriter(file));
 		br = new BufferedReader(new FileReader(file));
+
+//		getToday();
+//
+//		curStreak = calculateStreakLength();
+//
+//		if (!checkToday()) {
+//			bw.write(today);
+//		}
 	}
 
 	public void getToday() throws ParseException {
@@ -48,74 +46,111 @@ public class StreakCounter {
 	}
 
 	public void clear() {
-		
-		//search up how to clear a text file
+		// search up how to clear a text file
 	}
-	
-	public int calculateStreakLength() throws IOException{
+
+	public int calculateStreakLength() throws IOException {
 		calendar = Calendar.getInstance();
 		calendar.setTime(todaysDate);
-		
+
 		calendar.add(Calendar.DATE, -1);
 		dayBefore = calendar.getTime();
-		
+
 		int counter = 1;
-		
+
 		while ((str = br.readLine()) != null) {
-			while(str.contains(dayBefore+"")) {
+			System.out.println(dayBefore);
+			while (str.contains(dayBefore + "")) {
 				counter++;
 				calendar.add(Calendar.DATE, -1);
 				dayBefore = calendar.getTime();
+				System.out.println(dayBefore);
 			}
 		}
+
 		
-		if (counter == 1) {
-			clear();
-			bw.write(today);
-		}
-		
+//		if (counter == 1) {
+//			clear();
+//			bw = new BufferedWriter(new FileWriter(file));
+//			bw.write(today);
+//			bw.close();
+//		}
+
 		return counter;
 	}
-	
-	public boolean checkToday() throws IOException{
-		
+
+	public boolean checkToday() throws IOException {
+
 		while ((str = br.readLine()) != null) {
-			if(str.contains(today)) {
+			if (str.contains(today)) {
 				return true;
 			}
 		}
-		
 		return false;
 	}
 
-
 	public static void main(String[] args) throws ParseException, IOException {
-		//Date date = sdf.parse(today);
-
-
 		
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(todaysDate);
+		StreakCounter streak = new StreakCounter();
 		
+		streak.getToday();
+		
+		System.out.println(streak.calculateStreakLength());
+		
+		//Thu Jan 11 00:00:00 EST 2024Wed Jan 10 00:00:00 EST 2024Tue Jan 09 00:00:00 EST 2024
+		
+//		
+//		file = new File("loginStreak.txt").getAbsoluteFile();
+//		bw = new BufferedWriter(new FileWriter(file));
+//		br = new BufferedReader(new FileReader(file));
+//		
+//		today = dtf.format(LocalDateTime.now());
+//		todaysDate = sdf.parse(today);
+//		today = todaysDate + "";
+//		
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.setTime(todaysDate);
+//		bw.write(todaysDate+"");
+//		
 //		calendar.add(Calendar.DATE, -1);
 //		Date oneday = calendar.getTime();
+//		bw.write(oneday+"");
 //		
 //		calendar.add(Calendar.DATE, -1);
 //		Date twoday = calendar.getTime();
-
-		System.out.println(todaysDate);
-//		System.out.println(oneday);
-//		System.out.println(twoday);
-
-//		file = new File("loginStreak.txt").getAbsoluteFile();
-		BufferedReader br = new BufferedReader(new FileReader(file));
-
+//		bw.write(twoday+"");
+//		
+//		bw.close();
+		
 		String st;
 
 		while ((st = br.readLine()) != null) {
 			System.out.println(st);
 		}
-		
-		br.close();
+//		
+//		
+//		Date date = sdf.parse(today);
+//		
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.setTime(todaysDate);
+//		
+//		calendar.add(Calendar.DATE, -1);
+//		Date oneday = calendar.getTime();
+//		calendar.add(Calendar.DATE, -1);
+//		Date twoday = calendar.getTime();
+//		System.out.println(todaysDate);
+//		System.out.println(oneday);
+//		System.out.println(twoday);
+//		
+//		file = new File("loginStreak.txt").getAbsoluteFile();
+//		BufferedReader br = new BufferedReader(new FileReader(file));
+//
+//		String st;
+//
+//		while ((st = br.readLine()) != null) {
+//			System.out.println(st);
+//		}
+//		
+//		br.close();
 	}
 }
