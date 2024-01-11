cf525209d29c76f20631ec1850a33a489ddbc3ed
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-11 16:54:53 -0500

    Add files via upload

--- Pomodoro.java
+++ Pomodoro.java
@@ -4,9 +4,15 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import java.awt.event.*;
 
 import javax.swing.Timer;
 
@@ -15,10 +21,10 @@ import javax.swing.Timer;
 public class Pomodoro extends JPanel implements ActionListener {
 
 	public static final int POMO_WIDTH = 350;
-	public static final int POMO_HEIGHT = 150;
+	public static final int POMO_HEIGHT = 180;
 
-	public static int POMO_X = 100;
-	public static int POMO_Y = 100;
+	public static int pomoX = 100;
+	public static int pomoY = 100;
 
 	JButton startTimer;
 	JButton pauseTimer;
@@ -29,6 +35,7 @@ public class Pomodoro extends JPanel implements ActionListener {
 	JButton longBreak;
 
 	public Container c;
+	public static int timerVis;
 
 	// kinda dont need thsi but wtv
 	public static Timer timer;
@@ -38,8 +45,15 @@ public class Pomodoro extends JPanel implements ActionListener {
 	public static long initial;
 	public long remaining;
 
-	public boolean play, wt, lb, sb = false;
+	public boolean play, wt, lb, sb, playedAlert = false;
 
+	public int firstMouseX;
+	public int firstMouseY;
+	public int firstPomoX;
+	public int firstPomoY;
+	
+	public boolean mouseDragging;
+	
 	public Pomodoro(Container c) {
 		this.c = c;
 		// t = new Timer();
@@ -51,6 +65,7 @@ public class Pomodoro extends JPanel implements ActionListener {
 		initial = System.currentTimeMillis();
 
 		play = false;
+		timerVis = -1;
 
 		startTimer = new JButton("START");
 		pauseTimer = new JButton("PAUSE");
@@ -62,30 +77,19 @@ public class Pomodoro extends JPanel implements ActionListener {
 		longBreak = new JButton("long break");
 
 		c.add(startTimer);
-		startTimer.setBounds(105, 110, 75, 30);
-
 		c.add(pauseTimer);
-		pauseTimer.setBounds(105, 140, 75, 30);
-
 		c.add(resetTimer);
-		resetTimer.setBounds(105, 170, 75, 30);
 
 		c.add(changeAlert);
-		changeAlert.setBounds(340, 220, 110, 30);
 
 		c.add(workTime);
-		workTime.setBounds(340, 105, 100, 30);
-
 		c.add(shortBreak);
-		shortBreak.setBounds(340, 135, 100, 30);
-
 		c.add(longBreak);
-		longBreak.setBounds(340, 165, 100, 30);
 
 		startTimer.addActionListener(this);
 		pauseTimer.addActionListener(this);
 		resetTimer.addActionListener(this);
-		
+
 		changeAlert.addActionListener(this);
 
 		workTime.addActionListener(this);
@@ -95,22 +99,71 @@ public class Pomodoro extends JPanel implements ActionListener {
 	}
 
 	public void draw(Graphics g) {
-		g.drawRect(POMO_X, POMO_Y, POMO_WIDTH, POMO_HEIGHT);
 
-		g.setColor(Color.pink);
-		g.setFont(new Font("Times New Roman", Font.PLAIN, 60));
-		if (sec < 10) {
-			g.drawString(min + ":0" + sec, 190, 170);
-		} else {
-			g.drawString(min + ":" + sec, 190, 170);
-		}
-		if (play && System.currentTimeMillis() - initial >= 1000 && (min > 0 || sec > 0)) {
-			initial = System.currentTimeMillis();
-			sec--;
-			if (sec < 0) {
-				sec = 59;
-				min--;
+		if (timerVis == 1) {
+			g.drawRect(pomoX, pomoY, POMO_WIDTH, POMO_HEIGHT);
+			g.setColor(Color.PINK);
+			g.fillRect(pomoX, pomoY, POMO_WIDTH, 30);
+			g.setColor(Color.black);
+			g.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+			g.drawString("TIMER", pomoX+20, pomoY+20);
+			
+			startTimer.setVisible(true);
+			startTimer.setBounds(pomoX + 5, pomoY + 40, 75, 30);
+			
+			pauseTimer.setVisible(true);
+			pauseTimer.setBounds(pomoX + 5, pomoY + 70, 75, 30);
+			
+			resetTimer.setVisible(true);
+			resetTimer.setBounds(pomoX + 5, pomoY + 110, 75, 30);
+			
+			changeAlert.setVisible(true);
+			changeAlert.setBounds(pomoX + 240, pomoY + 150, 110, 30);
+			
+			workTime.setVisible(true);
+			workTime.setBounds(pomoX + 240, pomoY + 35, 100, 30);
+			
+			shortBreak.setVisible(true);
+			shortBreak.setBounds(pomoX + 240, pomoY + 65, 100, 30);
+			
+			longBreak.setVisible(true);
+			longBreak.setBounds(pomoX + 240, pomoY + 95, 100, 30);
+
+			g.setColor(Color.pink);
+			g.setFont(new Font("Times New Roman", Font.PLAIN, 60));
+
+			if (sec < 10) {
+				g.drawString(min + ":0" + sec, pomoX+90, pomoY+100);
+			} else {
+				g.drawString(min + ":" + sec, pomoX+90, pomoY+100);
 			}
+
+			if (sec == 0 && min == 0 && !playedAlert) {
+				play = false;
+				playSFX("alert1.wav");
+				g.drawString("0:00", 190, 170);
+				repaint();
+				playedAlert = true;
+			}
+
+			if (play && System.currentTimeMillis() - initial >= 1000 && (min > 0 || sec > 0)) {
+				initial = System.currentTimeMillis();
+				sec--;
+				if (sec < 0) {
+					sec = 59;
+					min--;
+				}
+			}
+		} else {
+			startTimer.setVisible(false);
+			pauseTimer.setVisible(false);
+			resetTimer.setVisible(false);
+			changeAlert.setVisible(false);
+			workTime.setVisible(false);
+			shortBreak.setVisible(false);
+			longBreak.setVisible(false);
+
+			play = false;
 		}
 
 	}
@@ -119,10 +172,11 @@ public class Pomodoro extends JPanel implements ActionListener {
 
 		if (evt.getSource() == startTimer) {
 			play = true;
+			playedAlert = false;
 		} else if (evt.getSource() == pauseTimer) {
 			play = false;
 		} else if (evt.getSource() == resetTimer) {
-			//idk why ths doesnt work, it doesnt seem to register when its clicked
+			// idk why ths doesnt work, it doesnt seem to register when its clicked
 			if (wt) {
 				min = 40;
 			} else if (sb) {
@@ -136,34 +190,79 @@ public class Pomodoro extends JPanel implements ActionListener {
 		} else if (evt.getSource() == workTime) {
 			min = 40;
 			sec = 0;
-			
-			wt= true;
+
+			wt = true;
 			sb = false;
 			lb = false;
-			
+
 			play = false;
 		} else if (evt.getSource() == shortBreak) {
-			min = 5;
-			sec = 0;
-			
-			wt= false;
+			min = 0;
+			sec = 10;
+
+			wt = false;
 			sb = true;
 			lb = false;
-			
+
 			play = false;
 		} else if (evt.getSource() == longBreak) {
 			min = 15;
 			sec = 0;
-			
-			wt= false;
+
+			wt = false;
 			sb = false;
 			lb = true;
-			
+
 			play = false;
-			
+
 		}
 
 		repaint();
+	}
+
+	// plays sound effects
+	public static void playSFX(String fileName) {
+
+		// try-catch to prevent crashing
+		try {
+
+			File songClip = new File(fileName);
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(songClip);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioStream);
+
+			// start playing
+			clip.start();
+
+			Thread.sleep(4000);
+			clip.stop();
+
+		} catch (Exception e) {
+			// nothing will happen if crashes
+		}
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+		firstMouseX = e.getX();
+		firstMouseY = e.getY();
+		firstPomoX = pomoX;
+		firstPomoY = pomoY;
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		if (firstMouseX >= firstPomoX && firstMouseX <= firstPomoX + POMO_WIDTH && firstMouseY >= firstPomoY
+				&& firstMouseY <= firstPomoY + 30) {
+			mouseDragging = true;
+		} else {
+			mouseDragging = false;
+		}
+
+		if (mouseDragging) {
+			pomoX = e.getX() - (firstMouseX - firstPomoX);
+			pomoY = e.getY() - (firstMouseY - firstPomoY);
+			repaint();
+		}
 	}
 
 }
