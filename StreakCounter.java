3f93f2a5e19704569c7ccaf12e06707cdaba1e87
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-15 17:38:46 -0500

    IT WORKS NOW WOOHOOOOO

--- StreakCounter.java
+++ StreakCounter.java
@@ -6,168 +6,100 @@
 */
 
 //import statements
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.io.*;
 import java.util.*;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class StreakCounter {
 
-	//variable declarations for file reading and writing
 	public static File file;
 	public static BufferedWriter bw;
 	public static BufferedReader br;
 
-	//variable declarations for formatting
 	public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-	//variable delcarations for logistics
-	public static Date todaysDate;
-	public static String today;
+	public Date todaysDate;
+	public Date curDate;
 	public String str;
 
 	public Date dayBefore;
-	public Calendar calendar;
+	public static Calendar calendar;
 
 	public int curStreak;
-	public StreakCounter streak;
 
-	//constructor
-	public StreakCounter() throws ParseException, IOException {
-
-		//variable initializations
+	// constructor
+	public StreakCounter() {
 		file = new File("loginStreak.txt").getAbsoluteFile();
-		bw = new BufferedWriter(new FileWriter(file));
-		br = new BufferedReader(new FileReader(file));
-
-//		getToday();
-//
-//		curStreak = calculateStreakLength();
-//
-//		if (!checkToday()) {
-//			bw.write(today);
-//		}
-	}
-
-	//get todays date and saves to variables
-	public void getToday() throws ParseException {
-		today = dtf.format(LocalDateTime.now());
-		todaysDate = sdf.parse(today);
-		today = todaysDate + "";
-	}
-
-	//unfinished method
-	public void clear() {
-		// search up how to clear a text file
-	}
-
-	//this method *should* calculate the streak length (doesn't work yet; needs troubleshooting)
-	public int calculateStreakLength() throws IOException {
 		calendar = Calendar.getInstance();
-		calendar.setTime(todaysDate);
+	}
 
+	// get todays date and saves to variables
+	public Date getToday() {
+		try {
+			curDate = sdf.parse(dtf.format(LocalDateTime.now()));
+		} catch (Exception e) {
+
+		}
+		return curDate;
+	}
+
+	// this method calculates the streak length
+	public int calculateStreakLength() {
+		int counter = 0;
+
+		calendar.setTime(todaysDate);
 		calendar.add(Calendar.DATE, -1);
 		dayBefore = calendar.getTime();
 
-		int counter = 1;
-
-		while ((str = br.readLine()) != null) {
-			System.out.println(dayBefore);
-			while (str.contains(dayBefore + "")) {
-				counter++;
-				calendar.add(Calendar.DATE, -1);
-				dayBefore = calendar.getTime();
-				System.out.println(dayBefore);
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while ((str = br.readLine()) != null) {
+				while (str.contains(dayBefore + "")) {
+					counter++;
+					calendar.add(Calendar.DATE, -1);
+					dayBefore = calendar.getTime();
+				}
 			}
+			br.close();
+		} catch (Exception e) {
+
 		}
 
-		
-//		if (counter == 1) {
-//			clear();
-//			bw = new BufferedWriter(new FileWriter(file));
-//			bw.write(today);
-//			bw.close();
-//		}
-
-		return counter;
+		return counter + 1;
 	}
 
-//	public boolean checkToday() throws IOException {
-//
-//		while ((str = br.readLine()) != null) {
-//			if (str.contains(today)) {
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
+	public void writeDates() {
+		calendar.setTime(todaysDate);
 
+		try {
+			bw = new BufferedWriter(new FileWriter(file));
+			bw.write(todaysDate + "");
+			for (int i = 0; i < (curStreak - 1); i++) {
+				calendar.add(Calendar.DATE, -1);
+				dayBefore = calendar.getTime();
+				bw.write(dayBefore + "");
+			}
+			bw.close();
+		} catch (Exception e) {
+
+		}
+	}
 	
-	// testing/troubleshooting code (that we cannot delete right now)
-//	public static void main(String[] args) throws ParseException, IOException {
-//		
-//		StreakCounter streak = new StreakCounter();
-//		
-//		streak.getToday();
-//		
-//		System.out.println(streak.calculateStreakLength());
-//		
-//		Thu Jan 11 00:00:00 EST 2024Wed Jan 10 00:00:00 EST 2024Tue Jan 09 00:00:00 EST 2024
-//		
-//		file = new File("loginStreak.txt").getAbsoluteFile();
-//		bw = new BufferedWriter(new FileWriter(file));
-//		br = new BufferedReader(new FileReader(file));
-//		
-//		today = dtf.format(LocalDateTime.now());
-//		todaysDate = sdf.parse(today);
-//		today = todaysDate + "";
-//		
-//		Calendar calendar = Calendar.getInstance();
-//		calendar.setTime(todaysDate);
-//		bw.write(todaysDate+"");
-//		
-//		calendar.add(Calendar.DATE, -1);
-//		Date oneday = calendar.getTime();
-//		bw.write(oneday+"");
-//		
-//		calendar.add(Calendar.DATE, -1);
-//		Date twoday = calendar.getTime();
-//		bw.write(twoday+"");
-//		
-//		bw.close();
-//		
-//		String st;
-//
-//		while ((st = br.readLine()) != null) {
-//			System.out.println(st);
-//		}
-//		
-//		
-//		Date date = sdf.parse(today);
-//		
-//		Calendar calendar = Calendar.getInstance();
-//		calendar.setTime(todaysDate);
-//		
-//		calendar.add(Calendar.DATE, -1);
-//		Date oneday = calendar.getTime();
-//		calendar.add(Calendar.DATE, -1);
-//		Date twoday = calendar.getTime();
-//		System.out.println(todaysDate);
-//		System.out.println(oneday);
-//		System.out.println(twoday);
-//		
-//		file = new File("loginStreak.txt").getAbsoluteFile();
-//		BufferedReader br = new BufferedReader(new FileReader(file));
-//
-//		String st;
-//
-//		while ((st = br.readLine()) != null) {
-//			System.out.println(st);
-//		}
-//		
-//		br.close();
-//	}
+	public void draw(Graphics g) {
+		if (todaysDate != getToday()) {
+			todaysDate = getToday();
+			curStreak = calculateStreakLength();
+			writeDates();
+		} 
+		g.setColor(Color.pink);
+		g.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		g.drawString("Current Streak: "+curStreak, 10, 635);
+	}
+
 }
