388372db9df19948ce6bca7a117256b2bd283cb1
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-17 22:38:22 -0500

    Update Tasklist.java comments

--- Tasklist.java
+++ Tasklist.java
@@ -4,12 +4,10 @@
  * This class creates a checklist of tasks, which can be added, edited, or removed (yet to be implemented)
 */
 
+//import statements
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.util.*;
-
 import javax.swing.*;
 
 public class Tasklist extends JPanel implements ActionListener {
