b37f231104ad731e0731e9f53959f79922f1dc7a
elaine! <110132709+elainesyqian@users.noreply.github.com>
2024-01-21 21:05:25 -0500

    comment for final

--- TomoMenu.java
+++ TomoMenu.java
@@ -14,15 +14,16 @@ import java.util.*;
 import javax.sound.sampled.*;
 import javax.swing.*;
 
+// TomoMenu constructor
 public class TomoMenu extends JPanel implements ActionListener {
 	// size of the rectangle
 	public static final int MENU_WIDTH = 300;
 	public static final int MENU_HEIGHT = 256;
-	
+
 	// saves the coordinates of the menu
 	public int menuX = 850;
 	public int menuY = 50;
-	
+
 	public int firstMouseX;
 	public int firstMouseY;
 	public int firstMenuX;
@@ -50,9 +51,6 @@ public class TomoMenu extends JPanel implements ActionListener {
 	public TomoFrame frame;
 	public String currentBg;
 	public static String todaysQuote;
-
-	// variables for visibility/sound of each room
-	public int rainVis, cafeVis, fireVis, birdVis, waveVis = -1;
 	public int counter = 0;
 
 	// all variables needed to play ambient sound
@@ -63,18 +61,24 @@ public class TomoMenu extends JPanel implements ActionListener {
 	// all variables needed to change background image
 	public ImageIcon rainIcon, cafeIcon, fireIcon, birdIcon, waveIcon, timerSymbol, listSymbol, stickySymbol;
 	public Image rainImage, cafeImage, fireImage, birdImage, waveImage;
-	
-	//variables for reading file
-    public static File file;
-    public static BufferedWriter bw;
-    public static BufferedReader br;
 
+	// array of room symbol for buttons
 	public ImageIcon[] backGroundSymbols = {
 			new ImageIcon(new ImageIcon("rainSymbol.png").getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH)),
 			new ImageIcon(new ImageIcon("cafeSymbol.png").getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH)),
 			new ImageIcon(new ImageIcon("fireSymbol.png").getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH)),
 			new ImageIcon(new ImageIcon("birdsSymbol.png").getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH)),
 			new ImageIcon(new ImageIcon("wavesSymbol.png").getImage().getScaledInstance(45, 45, Image.SCALE_SMOOTH)) };
+
+	// variables for reading file
+	public static File file;
+	public static BufferedWriter bw;
+	public static BufferedReader br;
+
+	// variable declarations for daily quote
+	public static String inputLine;
+	public static String quote = "";
+	public static int count;
 
 	// constructor of TomoMenu
 	public TomoMenu(Container c, TomoFrame frame) {
@@ -84,20 +88,20 @@ public class TomoMenu extends JPanel implements ActionListener {
 
 		// setup variables
 		currentBg = "Rain";
-		rainVis = 1;
-		
+
 		// getting the file with the saved sticky notes
 		file = new File("notes.txt").getAbsoluteFile();
 		// reading all the saved notes
-        readSaved();
+		readSaved();
 
+		// creates all the images for the timer, tasklist, and stickyNote icons
 		timerSymbol = new ImageIcon(
 				new ImageIcon("timerSymbol.png").getImage().getScaledInstance(73, 73, Image.SCALE_SMOOTH));
 		listSymbol = new ImageIcon(
 				new ImageIcon("listSymbol.png").getImage().getScaledInstance(73, 73, Image.SCALE_SMOOTH));
 		stickySymbol = new ImageIcon(
 				new ImageIcon("stickySymbol.png").getImage().getScaledInstance(73, 73, Image.SCALE_SMOOTH));
-		
+
 		// creates, and adds the timer button on the screen and adds an action
 		// listener
 		timerButton = new JButton("TIMER");
@@ -126,7 +130,7 @@ public class TomoMenu extends JPanel implements ActionListener {
 		newNotesButton.setFocusable(false);
 		newNotesButton.setMargin(new Insets(20, 32, 20, 20));
 		newNotesButton.setIcon(stickySymbol);
-				
+
 		// adds buttons for different rooms
 		for (JButton jB : backGroundButtons) {
 			c.add(jB);
@@ -187,23 +191,22 @@ public class TomoMenu extends JPanel implements ActionListener {
 
 		waveIcon = new ImageIcon("WaveBG.png");
 		waveImage = waveIcon.getImage();
-
 	}
 
 	// draws parts of the menu, the background, and controls ambient sound
 	public void draw(Graphics g) {
-		
+
 		// positions timer, checklist, and new note button on the main menu
 		timerButton.setBounds(menuX + 25, menuY + 55, 73, 73);
 		checkListButton.setBounds(menuX + 115, menuY + 55, 73, 73);
 		newNotesButton.setBounds(menuX + 207, menuY + 55, 73, 73);
-		
+
 		// positions the background changing buttons so that they are all beside each
 		// other
 		buttonRightShift = 0;
-				
+
 		for (JButton jB : backGroundButtons) {
-			//positions the each backGroundButton onto the main menu
+			// positions the each backGroundButton onto the main menu
 			jB.setBounds(menuX + 20 + buttonRightShift, menuY + 178, 45, 45);
 			// increases the x-position of the next background button to place
 			buttonRightShift = buttonRightShift + 55;
@@ -230,18 +233,18 @@ public class TomoMenu extends JPanel implements ActionListener {
 		// draws the draggable tab rectangle
 		g.setColor(Color.PINK);
 		g.fillRect(menuX, menuY, MENU_WIDTH, 30);
-		
+
 		// sets the font for the title
 		g.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		
+
 		// adds the title
 		g.setColor(Color.BLACK);
 		g.drawString("MENU", menuX + 10, menuY + 21);
-		
+
 		// draws the main rectangle
 		g.setColor(Color.WHITE);
 		g.fillRect(menuX, menuY + 30, MENU_WIDTH, MENU_HEIGHT);
-		
+
 		// sets the font for the rest of the text
 		g.setFont(new Font("Tahoma", Font.PLAIN, 15));
 
@@ -256,7 +259,6 @@ public class TomoMenu extends JPanel implements ActionListener {
 
 		// draws the respective room text to screen
 		// plays corresponding audio while stopping all other audio
-
 		try {
 
 			if (currentBg.equals("Rain")) {
@@ -325,6 +327,8 @@ public class TomoMenu extends JPanel implements ActionListener {
 			// nothing will happen if crashes
 		}
 
+		// draw each note in the array on screen if it isn't marked for deletion
+		// otherwise remove it from the array of notes
 		for (int i = 0; i < notes.size(); i++) {
 			if (!notes.get(i).delete) {
 				notes.get(i).draw(g);
@@ -333,60 +337,44 @@ public class TomoMenu extends JPanel implements ActionListener {
 			}
 		}
 
+		// save the sticky notes
 		writeToSave();
-		
+
 	}
 
+	// this method takes care all logistics when a button is clicked
 	public void actionPerformed(ActionEvent evt) {
 
+		// if the timer button is clicked, toggle timer visibility
 		if (evt.getSource() == timerButton) {
 			Pomodoro.timerVis = Pomodoro.timerVis * -1;
+
+			// if the checklist button is clicked, toggle checklist visibility
 		} else if (evt.getSource() == checkListButton) {
 			Tasklist.taskVis = Tasklist.taskVis * -1;
-		} else if (evt.getSource() == newNotesButton) {
+
 			// if the add a new note button was clicked, create a new sticky note
+		} else if (evt.getSource() == newNotesButton) {
 			newBlankNote = new StickyNotes(c, frame);
 			notes.add(newBlankNote);
+
+			// if a background button was clicked, set the currentBg to
+			// the corresponding background
 		} else if (evt.getSource() == backGroundButtons[0]) {
-			rainVis = rainVis * -1;
 			currentBg = "Rain";
 
-			cafeVis = -1;
-			fireVis = -1;
-			birdVis = -1;
-			waveVis = -1;
 		} else if (evt.getSource() == backGroundButtons[1]) {
-			cafeVis = cafeVis * -1;
 			currentBg = "Cafe";
 
-			rainVis = -1;
-			fireVis = -1;
-			birdVis = -1;
-			waveVis = -1;
 		} else if (evt.getSource() == backGroundButtons[2]) {
-			fireVis = fireVis * -1;
 			currentBg = "Fire";
 
-			rainVis = -1;
-			cafeVis = -1;
-			birdVis = -1;
-			waveVis = -1;
 		} else if (evt.getSource() == backGroundButtons[3]) {
-			birdVis = birdVis * -1;
 			currentBg = "Bird";
 
-			rainVis = -1;
-			cafeVis = -1;
-			fireVis = -1;
-			waveVis = -1;
 		} else if (evt.getSource() == backGroundButtons[4]) {
-			waveVis = waveVis * -1;
 			currentBg = "Wave";
 
-			rainVis = -1;
-			cafeVis = -1;
-			birdVis = -1;
-			fireVis = -1;
 		}
 
 		repaint(); // update screen to show changes
@@ -399,7 +387,7 @@ public class TomoMenu extends JPanel implements ActionListener {
 		for (int i = 0; i < notes.size(); i++) {
 			notes.get(i).mousePressed(e);
 		}
-		
+
 		firstMouseX = e.getX();
 		firstMouseY = e.getY();
 		firstMenuX = menuX;
@@ -413,7 +401,7 @@ public class TomoMenu extends JPanel implements ActionListener {
 		for (int i = 0; i < notes.size(); i++) {
 			notes.get(i).mouseDragged(e);
 		}
-		
+
 		if (firstMouseX >= firstMenuX && firstMouseX <= firstMenuX + MENU_WIDTH && firstMouseY >= firstMenuY
 				&& firstMouseY <= firstMenuY + 30) {
 			// if the mouse was in the title tab, allow for the menu to be dragged
@@ -437,107 +425,120 @@ public class TomoMenu extends JPanel implements ActionListener {
 		}
 	}
 
+	// this method uses webscraping get the daily quote from a website
 	public static String getQuote() {
 
-		String inputLine;
-		String quote = "";
-		int counter = 0;
+		// reset count to 0
+		count = 0;
 
 		try {
+			// import link into bufferedReader to read HTML code
 			URL url = new URL("https://zenquotes.io/");
 			BufferedReader quoteReader = new BufferedReader(new InputStreamReader(url.openStream()));
 
+			// while next line is available
 			while ((inputLine = quoteReader.readLine()) != null) {
 
-				if (counter == 173) {
+				// increase counter by 1
+				count++;
+
+				// if counter is at line 174
+				// aka where the quote is stored in the code
+				if (count == 174) {
+					// edit the line of code so that we get a formatted quote
 					quote = inputLine.replaceFirst("        <h4 class=\"display-5 font-italic\"><blockquote>&ldquo;",
 							"");
 					quote = "\"" + quote.substring(0, quote.indexOf("<br"));
 					quote = quote.replaceAll(".&rdquo; &mdash; <footer>", "\" — ");
+					
+					//exits the while loop
+					break;
 				}
-				counter++;
+
 			}
+			// closes the quote reader
 			quoteReader.close();
 
 		} catch (Exception e) {
-
+			// nothing will happen if crashes
 		}
+		// returns the quote
 		return quote;
 	}
-	
+
 	// reads the saved information about the sticky notes
 	// and adds the saved notes to the array of sticky notes
 	public void readSaved() {
-        String str;
-        String allText = "";
-        StickyNotes newNote;
+		String str;
+		String allText = "";
+		StickyNotes newNote;
 
-        try {
-            //setup buffered reader to read from the txt file
-            br = new BufferedReader(new FileReader(file));
+		try {
+			// setup buffered reader to read from the txt file
+			br = new BufferedReader(new FileReader(file));
 
-            //while the next line has content
-            while ((str = br.readLine()) != null) {
-            	// if the line isn't a dividing character
-            	// which splits the content of the sticky notes from it's coordinates
-                if (!str.equals("÷")) {
-                    if (!allText.equals("")) {
-                    	// if the current text for the note isn't empty
-                    	// move to the next line and add the next part of the note
-                        allText = allText + "\n" + str;
-                    } else {
-                    	// otherwise, stay on the current line
-                    	// and add the next part of the note
-                        allText = allText + str;
-                    }
-                } else {
-                	// if the dividing character is reached
-                	// the next two lines are the coordinates of the sticky note
-                	
-                	// create a new note with the text read in
-                    newNote = new StickyNotes(c, frame);
-                    newNote.note.setText(allText);
-                    
-                    // use the next two lines, which contain the coordinates of the note
-                    // to position it on screen
-                    str = br.readLine();
-                    newNote.stickyX = Integer.parseInt(str);
-                    str = br.readLine();
-                    newNote.stickyY = Integer.parseInt(str);
-                    
-                    // add the newly creates note to the array list of sticky notes
-                    notes.add(newNote);
-                    
-                    // clear the text to add to the next note
-                    allText = "";
-                }
-            }
+			// while the next line has content
+			while ((str = br.readLine()) != null) {
+				// if the line isn't a dividing character
+				// which splits the content of the sticky notes from it's coordinates
+				if (!str.equals("÷")) {
+					if (!allText.equals("")) {
+						// if the current text for the note isn't empty
+						// move to the next line and add the next part of the note
+						allText = allText + "\n" + str;
+					} else {
+						// otherwise, stay on the current line
+						// and add the next part of the note
+						allText = allText + str;
+					}
+				} else {
+					// if the dividing character is reached
+					// the next two lines are the coordinates of the sticky note
 
-            //closes buffered reader
-            br.close();
-        } catch (Exception e) {
-            // nothing will happen if crashes
-        }
-    }
-	
+					// create a new note with the text read in
+					newNote = new StickyNotes(c, frame);
+					newNote.note.setText(allText);
+
+					// use the next two lines, which contain the coordinates of the note
+					// to position it on screen
+					str = br.readLine();
+					newNote.stickyX = Integer.parseInt(str);
+					str = br.readLine();
+					newNote.stickyY = Integer.parseInt(str);
+
+					// add the newly creates note to the array list of sticky notes
+					notes.add(newNote);
+
+					// clear the text to add to the next note
+					allText = "";
+				}
+			}
+
+			// closes buffered reader
+			br.close();
+		} catch (Exception e) {
+			// nothing will happen if crashes
+		}
+	}
+
 	// writes the content of the sticky notes and it's coordinates to a file
-    public void writeToSave() {
-        try {
-        	// creates a bufferedwriter to write content to the file
-            bw = new BufferedWriter(new FileWriter(file));
-            // for every sticky note in the array of notes
-            for (int i = 0; i < notes.size(); i++) {
-            	// write it's contents into a file
-            	//and end it with line with an untypeable character
-                bw.write(notes.get(i).note.getText() + "\n÷\n");
-                // save the x and y coordinates of the sticky note on the next two lines
-                bw.write(notes.get(i).stickyX + "\n" + notes.get(i).stickyY + "\n");
-            }
-            bw.close();
-            //closes the buffered reader
-        } catch (Exception e) {
-            // nothing will happen if crashes
-        }
-    }
+	public void writeToSave() {
+		try {
+			// creates a bufferedwriter to write content to the file
+			bw = new BufferedWriter(new FileWriter(file));
+			// for every sticky note in the array of notes
+			for (int i = 0; i < notes.size(); i++) {
+				// write it's contents into a file
+				// and end it with line with an untypeable character
+				bw.write(notes.get(i).note.getText() + "\n÷\n");
+				// save the x and y coordinates of the sticky note on the next two lines
+				bw.write(notes.get(i).stickyX + "\n" + notes.get(i).stickyY + "\n");
+			}
+			bw.close();
+			// closes the buffered reader
+		} catch (Exception e) {
+			// nothing will happen if crashes
+		}
+	}
 
 }
